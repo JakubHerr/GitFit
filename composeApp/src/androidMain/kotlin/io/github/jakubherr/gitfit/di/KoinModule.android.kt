4a8d7961c898341740f41687e71ade3a3c4eccbe
Jakub Herrmann <kuba2c@seznam.cz>
2025-04-08 21:53:47 +0200

    Add more translations

--- composeApp/src/androidMain/kotlin/io/github/jakubherr/gitfit/di/KoinModule.android.kt
+++ composeApp/src/androidMain/kotlin/io/github/jakubherr/gitfit/di/KoinModule.android.kt
@@ -1,9 +1,7 @@
 package io.github.jakubherr.gitfit.di
 
-import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 actual val platformModule =
     module {
-        single { Localization(context = androidContext()) }
     }
