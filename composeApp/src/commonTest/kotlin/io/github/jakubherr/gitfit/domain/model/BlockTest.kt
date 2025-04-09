4f7d0aeae2a8722f74902f7e003c9d0994ef700e
Jakub Herrmann <kuba2c@seznam.cz>
2025-04-09 15:24:02 +0200

    Remove redundant and outdated tests

--- composeApp/src/commonTest/kotlin/io/github/jakubherr/gitfit/domain/model/BlockTest.kt
+++ composeApp/src/commonTest/kotlin/io/github/jakubherr/gitfit/domain/model/BlockTest.kt
@@ -1,14 +1,13 @@
 package io.github.jakubherr.gitfit.domain.model
 
 import kotlin.test.Test
-import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class BlockTest {
     private val testExercise = Exercise("exerciseId", "exerciseName", primaryMuscle = MuscleGroup.CHEST)
 
     @Test
-    fun weightProgressesWhenItShould() {
+    fun weightProgressionWorks() {
         // given a completed block with valid weight progression settings that meet progression criteria
         val originalBlock = Block(
             idx = 0,
@@ -54,7 +53,7 @@ class BlockTest {
     }
 
     @Test
-    fun repetitionProgressesWhenItShould() {
+    fun repetitionProgressionWorks() {
         // given a completed block with valid repetition progression settings that meet progression criteria
         val originalBlock = Block(
             idx = 0,
@@ -95,79 +94,5 @@ class BlockTest {
         }
         // and progression setting repetition threshold is updated for the next progression
         assertTrue { updatedBlock.progressionSettings?.repThreshold == 12 }
-    }
-
-    @Test
-    fun weightStaysTheSameWhenItShould() {
-        // given a block where user did not meet progression criteria
-        val originalBlock = Block(
-            idx = 0,
-            exercise = testExercise,
-            series = listOf(
-                Series(
-                    idx = 0,
-                    repetitions = 10,
-                    weight = 25.0,
-                    completed = true
-                ),
-                Series(
-                    idx = 0,
-                    repetitions = 10,
-                    weight = 20.0,
-                    completed = true
-                )
-            ),
-            progressionSettings = ProgressionSettings(
-                incrementWeightByKg = 5.00,
-                type = ProgressionType.INCREASE_WEIGHT,
-                trigger = ProgressionTrigger.MINIMUM_REPS_AND_WEIGHT_EVERY_SET,
-                incrementRepsBy = 0,
-                weightThreshold = 44.0,
-                repThreshold = 10,
-            )
-        )
-
-        // when weight is progressed
-        val updatedBlock = originalBlock.progressWeight()
-
-        // then the block stays the same
-        assertEquals(updatedBlock, originalBlock)
-    }
-
-    @Test
-    fun repetitionsStayTheSameWhenTheyShould() {
-        // given a block where user did not meet progression criteria
-        val originalBlock = Block(
-            idx = 0,
-            exercise = testExercise,
-            series = listOf(
-                Series(
-                    idx = 0,
-                    repetitions = 10,
-                    weight = 25.0,
-                    completed = true
-                ),
-                Series(
-                    idx = 0,
-                    repetitions = 10,
-                    weight = 20.0,
-                    completed = true
-                )
-            ),
-            progressionSettings = ProgressionSettings(
-                incrementWeightByKg = 0.0,
-                type = ProgressionType.INCREASE_REPS,
-                trigger = ProgressionTrigger.MINIMUM_REPS_AND_WEIGHT_EVERY_SET,
-                incrementRepsBy = 2,
-                weightThreshold = 20.0,
-                repThreshold = 15,
-            )
-        )
-
-        // when repetitions are progressed
-        val updatedBlock = originalBlock.progressReps()
-
-        // then the block stays the same
-        assertEquals(originalBlock, updatedBlock)
     }
 }
