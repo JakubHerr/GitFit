ae8109972525952f796b109e5ee80da36a374ec4
Jakub Herrmann <kuba2c@seznam.cz>
2025-04-17 13:05:36 +0200

    Add automated tests for F02 and F06

--- composeApp/src/commonTest/kotlin/instrumented/Test.kt
+++ composeApp/src/commonTest/kotlin/instrumented/Test.kt
@@ -1,31 +1,67 @@
 package instrumented
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.runComposeUiTest
-import io.github.jakubherr.gitfit.domain.model.User
-import io.github.jakubherr.gitfit.presentation.auth.AuthState
-import io.github.jakubherr.gitfit.presentation.auth.LoginScreen
+import androidx.compose.ui.test.waitUntilDoesNotExist
+import io.github.jakubherr.gitfit.presentation.App
 import kotlin.test.Test
 
-class ExampleTest {
+class NavigationTest {
 
+    // this test validates use cases F02 and F06 with real database on a testing account
+    // It also navigates through all top level destinations in the app
+    // this test assumes that it is run on a real device with internet access with Czech localization
     @OptIn(ExperimentalTestApi::class)
     @Test
-    fun myTest() = runComposeUiTest {
+    fun navigationTest() = runComposeUiTest {
         setContent {
-            var authState by remember { mutableStateOf(AuthState(
-                User.LoggedOut,
-                null,
-                false,
-            )) }
-
-            LoginScreen(
-                authState
-            )
+            App()
         }
+
+        onNodeWithText("Zapomenuté heslo").performClick()
+        waitForIdle()
+
+        onNodeWithContentDescription("Navigovat zpět").performClick()
+        waitForIdle()
+
+        onNodeWithText("Email").performTextInput("uitest@test.test")
+        waitForIdle()
+
+        onNodeWithText("Heslo").performTextInput("uitestpassword")
+        waitForIdle()
+
+        onNodeWithText("Přihlásit se").performClick()
+
+        waitUntilDoesNotExist(
+            hasTestTag("AuthProgressIndicator"),
+            timeoutMillis = 3000
+        )
+
+        onNodeWithText("Přeskočit ověření").performClick()
+        waitForIdle()
+
+        onNodeWithText("Plány").performClick()
+        waitForIdle()
+
+        onNodeWithText("Měření").performClick()
+        waitForIdle()
+
+        onNodeWithText("Historie").performClick()
+        waitForIdle()
+
+        onNodeWithText("Nastavení").performClick()
+        waitForIdle()
+
+        onNodeWithText("Odhlásit se").performClick()
+
+        waitUntilDoesNotExist(
+            hasTestTag("AuthProgressIndicator"),
+            timeoutMillis = 3000
+        )
     }
 }
