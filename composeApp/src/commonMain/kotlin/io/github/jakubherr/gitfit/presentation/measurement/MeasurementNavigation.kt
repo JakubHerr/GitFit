338f1c63488b7aa358d4637799e956bcbc1e4996
Jakub Herrmann <kuba2c@seznam.cz>
2025-04-25 13:49:17 +0200

    Add missing empty screen prompt

--- composeApp/src/commonMain/kotlin/io/github/jakubherr/gitfit/presentation/measurement/MeasurementNavigation.kt
+++ composeApp/src/commonMain/kotlin/io/github/jakubherr/gitfit/presentation/measurement/MeasurementNavigation.kt
@@ -1,12 +1,19 @@
 package io.github.jakubherr.gitfit.presentation.measurement
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import gitfit.composeapp.generated.resources.Res
+import gitfit.composeapp.generated.resources.empty_measurement_history
 import gitfit.composeapp.generated.resources.error_invalid_measurement_values
 import io.github.jakubherr.gitfit.presentation.MeasurementAddEditRoute
 import io.github.jakubherr.gitfit.presentation.MeasurementHistoryRoute
@@ -14,6 +21,7 @@ import io.github.jakubherr.gitfit.presentation.MeasurementRoute
 import io.github.jakubherr.gitfit.presentation.sharedViewModel
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.getString
+import org.jetbrains.compose.resources.stringResource
 
 fun NavGraphBuilder.measurementNavigation(
     navController: NavHostController,
@@ -51,11 +59,21 @@ fun NavGraphBuilder.measurementNavigation(
         val vm = navController.sharedViewModel<MeasurementViewModel>()
         val measurements by vm.allUserMeasurements.collectAsStateWithLifecycle(emptyList())
 
-        MeasurementHistoryScreen(
-            measurements.sortedByDescending { it.date },
-            onDeleteMeasurement = {
-                vm.onAction(MeasurementAction.DeleteMeasurement(it))
-            },
-        )
+        if (measurements.isEmpty()) {
+            Column(
+                Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(stringResource(Res.string.empty_measurement_history))
+            }
+        } else {
+            MeasurementHistoryScreen(
+                measurements.sortedByDescending { it.date },
+                onDeleteMeasurement = {
+                    vm.onAction(MeasurementAction.DeleteMeasurement(it))
+                },
+            )
+        }
     }
 }
